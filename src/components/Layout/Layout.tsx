d687ed0633a82392399dc7e03f8ed11463789004
DESKTOP-KI4HRCJ\berna <bernardoribeiro99@gmail.com>
2024-01-13 17:07:38 +0000

    Fixed spinner styling and refresh timeout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { ReactNode, useContext } from 'react'
+import { ReactNode, useReducer, useContext, useRef } from 'react'
 import { BusDataContext } from 'Providers/BusDataProvider'
 import { BusDataProviderValue } from "types/types";
 import { IoIosRefresh } from "react-icons/io";
@@ -11,6 +11,22 @@ type Props = {
 
 const Layout = ({ children}: Props) => {
   const {busDataFetch} = useContext<BusDataProviderValue>(BusDataContext)
+  const isButtonDisabledRef = useRef(false);
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+
+  const handleButtonClick = () => {
+    if (!isButtonDisabledRef.current) {
+      isButtonDisabledRef.current = true;
+      busDataFetch();
+
+      // Enable the button after 5 seconds
+      setTimeout(() => {
+        isButtonDisabledRef.current = false;
+        forceUpdate()
+      }, 5000);
+    }
+  };
 
   return (
     <div>
@@ -18,7 +34,14 @@ const Layout = ({ children}: Props) => {
         <Link to='/' className='bg-sky-600 p-2 rounded text-white mr-1 w-32 text-center'>Autocarros</Link>
         <Link to='/lines' className='bg-sky-600 p-2 rounded text-white ml-1 mr-1 w-32 text-center'>Linhas</Link>
         <Link to='/rares' className='bg-sky-600 p-2 rounded text-white ml-1 mr-1 w-32 text-center'>Raridades</Link>
-        <IconButton className="bg-green-600 p-2 rounded text-white ml-1 w-12 flex justify-center items-center" onClick={busDataFetch} icon={IoIosRefresh} size={20} />
+        <IconButton
+          className={`bg-green-600 p-2 rounded text-white ml-1 w-12 flex justify-center items-center ${
+            isButtonDisabledRef.current ? 'opacity-50 cursor-not-allowed' : ''
+          }`}
+          onClick={handleButtonClick}
+          icon={IoIosRefresh}
+          size={20}
+        />
       </div>
       <div className="fit-screen">
         {children}
