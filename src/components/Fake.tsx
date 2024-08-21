8ffb56cd7b7c53f058d14cca1874c8fd2dd2b6f8
bernasx <bernardo.ribeiro99@gmail.com>
2024-08-21 10:49:04 +0100

    Revert "Added fake path to initial URL"
    This reverts commit 3df91fca54b003923f04ce7135dcba3166b84bc0.

--- src/components/Fake.tsx
+++
@@ -1,7 +0,0 @@
-import LoadingSpinner from './LoadingSpinner/LoadingSpiner';
-
-function Fake() {
-  return <LoadingSpinner/>
-}
-
-export default Fake
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -31,7 +31,7 @@ const Layout = ({ children}: Props) => {
   return (
     <div>
       <div className='flex justify-center align-center mt-2'>
-        <Link to='/autocarros' className='bg-slate-500 p-2 rounded text-white mr-1 w-32 text-center'>Autocarros</Link>
+        <Link to='/' className='bg-slate-500 p-2 rounded text-white mr-1 w-32 text-center'>Autocarros</Link>
         <Link to='/lines' className='bg-slate-500 p-2 rounded text-white ml-1 mr-1 w-32 text-center'>Linhas</Link>
         <Link to='/rares' className='bg-slate-500 p-2 rounded text-white ml-1 mr-1 w-32 text-center'>Raridades</Link>
         <IconButton
